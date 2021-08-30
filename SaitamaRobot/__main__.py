25f772c409921bcc5507f8219f6c7b8e090c515a
qwertytech123 <83151110+qwertytech123@users.noreply.github.com>
2021-08-30 19:02:16 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -72,7 +72,8 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hey hi {}, I'm {}!
-*I am an Anime themed advance group management bot with a lot of Sexy Features.*
+I am an Anime themed group management bot.
+Built by weebs for weebs, I specialize in managing anime eccentric communities!
 """
 
 HELP_STRINGS = """
